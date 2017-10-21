eddfaeaa4a440aa6c0f4f1cb449f8ec6e93a5b25
pmaslankowski <pmaslankowski@gmail.com>
2017-10-21 17:06:03 +0200

    Commit with remaining test files.

--- src/Application.java
+++ src/Application.java
@@ -1,5 +1,8 @@
 import districts.DistrictsRepository;
 import districts.DistrictsRepositoryLoader;
+import hospitals.HospitalsRepository;
+import hospitals.HospitalsRepositoryLoader;
+import hospitals.exceptions.HospitalsLoadingException;
 
 import javax.swing.*;
 
@@ -12,17 +15,30 @@ public class Application {
         frame.pack();
         frame.setVisible(true);
 
-        test();
+        testHospitalsRepository();
     }
 
-    private static void test() {
+    private static void testDistrictsRepository() {
         DistrictsRepositoryLoader loader = new DistrictsRepositoryLoader("data/districts/");
         try {
             loader.load();
             DistrictsRepository repo = new DistrictsRepository(loader);
             System.out.println(repo);
             System.out.println();
-            System.out.println(repo.get("Dworcowa", 20));
+            System.out.println(repo.get("Zdrowa", 20));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void testHospitalsRepository() {
+        HospitalsRepositoryLoader loader = new HospitalsRepositoryLoader("data/hospitals.xml");
+        try {
+            loader.load();
+            HospitalsRepository repo = new HospitalsRepository(loader);
+            System.out.println(repo);
+            System.out.println();
+            System.out.println(repo.get("TEST_HOSPITAL1").getAddress());
         } catch (Exception e) {
             e.printStackTrace();
         }
